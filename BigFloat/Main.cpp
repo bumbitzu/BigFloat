943805cf7feef8ef2f8d7266df423dc14261830b
Adrian Bumbu <adibumbu91@gmail.com>
2023-02-21 00:06:38 +0200

    Some changes

--- BigFloat/Main.cpp
+++ BigFloat/Main.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	try
 	{
+		//Finding pi
 		BigFloat circumference("100000"), diameter("31830.99"),pi;
-
 		set_precision(circumference, diameter,1000);
 		pi = circumference / diameter;
 		cout << char(-29) << " " << char(-9) << " ";
